f9372b42a61e18c3c230a8761237a8700e8f1d44
lggambasica <lggambasica@misena.edu.co>
2022-12-20 08:37:43 -0500

    odoo15

---
+++ clases_udemy/peliculas/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+{
+    "name": "modulo de peliculas",
+    "version": "1.0",
+    "depends": [
+        "contacts",
+    ],
+    "author": "Perez Gambasica",
+    "category": "peliculas",
+    "website": "http://www.google.com",
+    "summary": "modulo de presupuestos para peliculas",
+    "description": """
+    Modulo para hacer presupuestos de peliculas
+    """,
+    "data": [
+        'data/categoria.xml',
+        'data/secuencia.xml',
+        "views/presupuesto_views.xml",
+        "views/menu.xml",
+    ],
+}
